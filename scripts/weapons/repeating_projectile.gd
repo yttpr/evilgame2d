615b09b72ba8dd26fd02203f707b800619276903
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-08-09 15:26:53 -0700

    fvcadr

--- scripts/weapons/repeating_projectile.gd
+++ scripts/weapons/repeating_projectile.gd
@@ -6,21 +6,26 @@ extends MovingProjectile
 @export var interval : float
 @export var is_player : bool
 
+@export var original = true
+
 func _shoot(direction : Vector2, origin : Vector2) -> void:
-	super._shoot(direction, origin)
-	await get_tree().create_timer(interval).timeout
-	repeats -= 1
-	if repeats <= 0:
+	if !original:
+		super._shoot(direction, origin)
 		return
-	var copy : RepeatingProjectile = self.duplicate()
-	self.get_parent().add_child(copy)
-	copy.repeats = repeats
-	if is_player:
-		var mouse = to_global(get_local_mouse_position())
-		var dir = Manager.Player.weapon_handler.pointer.global_position.direction_to(mouse)
-		var loc = Manager.Player.weapon_handler.pointer.global_position
-		if Manager._check_in_wall(loc - Manager.Player.weapon_handler._get_offset_vector()):
-			loc = Manager.Player.weapon_handler.weapon.global_position
-		copy._shoot(dir, loc)
-	else:
-		copy._shoot(direction, origin)
+	
+	for i in repeats:
+		var copy : RepeatingProjectile = self.duplicate()
+		copy.original = false
+		self.get_parent().add_child(copy)
+		if is_player:
+			var mouse = to_global(get_local_mouse_position())
+			var dir = Manager.Player.weapon_handler.pointer.global_position.direction_to(mouse)
+			var loc = Manager.Player.weapon_handler.pointer.global_position
+			if Manager._check_in_wall(loc - Manager.Player.weapon_handler._get_offset_vector()):
+				loc = Manager.Player.weapon_handler.weapon.global_position
+			copy._shoot(dir, loc)
+		else:
+			copy._shoot(direction, origin)
+		
+		await get_tree().create_timer(interval).timeout
+	self.queue_free()
